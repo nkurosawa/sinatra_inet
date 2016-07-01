33a2a4f5b9afd3c58592fbdad49452aa23dad0da
naoya kurosawa <naoya kurosawa>
2016-07-01 20:49:49 +0900

    projects sinatra inet demo

---
+++ app.rb
@@ -0,0 +1,24 @@
+#require 'sinatra'
+#require 'sinatra/reloader'
+
+class App < Sinatra::Base
+  register Sinatra::Reloader
+
+  # get '/' do
+  #   haml :index
+  # end
+  post '/2' do
+    haml :index_2
+  end
+  get '/2' do
+    haml :index_2
+  end
+
+  # get '/1' do
+  #   haml :index1
+  # end
+  # get '/3' do
+  #   haml :index_3
+  # end
+
+end
